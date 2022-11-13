ef93cc7ae0bda4b1256f4b7d1795b4c4ff22aa4b
Wisdom A Kwabena <linxford7@gmail.com>
2022-11-13 21:01:12 +0000

    license

---
+++ tutship/cp.js
@@ -0,0 +1,6 @@
+const site = "NetVally.com and its associates";
+const info = "Licensed for and by Wisdak for all his Projects - Copyright Protected";
+var link = "Chat him on ==> https://www.t.me/wisdak1"; 
+console.log(site)
+console.log(info)
+console.log(link)
