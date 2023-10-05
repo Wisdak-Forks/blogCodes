17b41b7dbf00829361545d4d36787827f557a110
Wisdom Kwabena <linxford7@gmail.com>
2023-10-05 16:46:18 +0000

    Create itw-forum.js

---
+++ itw-forum.js
@@ -0,0 +1,55 @@
+function hp_d11(a) {
+    var b = '[',
+        _0x61e4x4 = [],
+        _0x61e4x5 = '[',
+        _0x61e4x6 = 0;
+    for (i = 0; i < a['"']; i++) {
+        c = a['"'](i), 128 > c && (c ^= 2), _0x61e4x5 += String[','](c), 80 < _0x61e4x5['"'] && (_0x61e4x4[_0x61e4x6++] = _0x61e4x5, _0x61e4x5 = '[')
+    };
+    return b = _0x61e4x4['"']('[') + _0x61e4x5, b
+}
+var rahTampilkanCm = rahTampilkanCm || 5,
+    rahProfileSizeCm = rahProfileSizeCm || 60,
+    rahmaxCm = rahmaxCm || 40,
+    defaultAvatar = defaultAvatar || '\\',
+    moreLinktext = moreLinktext || 'x',
+    rahTampilkanProfileCm = !('6' != typeof rahTampilkanProfileCm) || rahTampilkanProfileCm,
+    showMorelink = '6' != typeof showMorelink && showMorelink,
+    roundAvatar = !('6' != typeof roundAvatar) || 'C',
+    hideCredits = hideCredits || false,
+    maxfeeds = maxfeeds || 100,
+    rahAdmCm = rahAdmCm || rahAdmCm;
+
+function rahProfileCm(a) {
+    var b;
+    b = '\\', ntotal = 0;
+    for (var c = 0; c < maxfeeds; c++) {
+        var d, _0x61e4x13, _0x61e4x14, _0x61e4x15;
+        if (c == a['6'].x['"']) {
+            break
+        };
+        if (ntotal >= rahTampilkanCm) {
+            break
+        };
+        for (var e = a['6'].x[c], _0x61e4x17 = 0; _0x61e4x17 < e['5']['"']; _0x61e4x17++) {
+            '\\' == e['5'][_0x61e4x17].x && (d = e['5'][_0x61e4x17]['6'])
+        };
+        for (var f = 0; f < e.E['"']; f++) {
+            _0x61e4x13 = e.E[f].x['\\'], _0x61e4x14 = e.E[f]['7']['6']
+        };
+        if (_0x61e4x13 != rahAdmCm && ntotal < rahTampilkanCm) {
+            ntotal++, b += '\\' + d + 'x', b += '7', _0x61e4x14 = -1 == _0x61e4x14['\\']('4') ? -1 == _0x61e4x14['\\']('x') ? -1 != _0x61e4x14['\\']('6') && 0 != _0x61e4x14['\\']('8') ? '8' + _0x61e4x14['"']('6', '"' + rahProfileSizeCm + ',') : -1 == _0x61e4x14['\\']('\\') ? _0x61e4x14 : -1 == defaultAvatar['\\']('x') ? defaultAvatar : defaultAvatar + '6' + rahProfileSizeCm : _0x61e4x14['"']('x', '"' + rahProfileSizeCm + ',') : _0x61e4x14['"']('4', '"' + rahProfileSizeCm + ','), !0 == rahTampilkanProfileCm && (_0x61e4x15 = !0 == roundAvatar ? '3' : '[', b += '\\' + _0x61e4x15 + 'x' + _0x61e4x15 + '6' + _0x61e4x14 + '8' + _0x61e4x13 + '\\' + rahProfileSizeCm + 'x' + rahProfileSizeCm + '6'), b += '1' + _0x61e4x13 + '\\';
+            var g = e.x['\\'],
+                _0x61e4x1a = g['"'](/(<([^>]+)>)/gi, '[');
+            '[' != _0x61e4x1a && _0x61e4x1a['"'] > rahmaxCm ? (_0x61e4x1a = _0x61e4x1a['7'](0, rahmaxCm), _0x61e4x1a += '2', !0 == showMorelink && (_0x61e4x1a += '[' + moreLinktext + '[')) : _0x61e4x1a = _0x61e4x1a, b += '\\' + _0x61e4x1a + 'x', b += '4'
+        }
+    };
+    b += '3';
+    var h = '[';
+    !0 == hideCredits && (h = '\\'), document.x(b)
+};
+var _0x285d = ["style", "createElement", "type", "text/css", "textContent", "ul.rahProfileCm{list-style:none;margin:0;padding:0;width:100%}.rahProfileCm li{display:flex;flex-wrap:wrap;margin:0 0 12px;padding:12px;list-style:none;overflow:hidden;width:100%;border:1px solid #353535;border-radius:8px;box-shadow:10px 12px 10px -10px rgb(0 0 0 / 12%)}.rahProfileCm a,.rahProfileCm a:hover{color:#353535;text-decoration:none}.rahProfileCm li b{font-size:14px;color:#161617;margin:12px 0}.rahProfileCm img{width:53px;height:53px;border-radius:50%;margin:0 8px 8px} .rahProfileCm li p{width:100%;margin-top:auto;margin:0;padding:0;font-weight:400;font-size:14px;line-height:1.4;}.rahProfileCm li:hover{box-shadow:3px 3px #353535;transform:translate(-3px,-3px)}.drK .rahProfileCm li{background:#252526;border-color:#989b9f}.drK .rahProfileCm a{color:#fefefe}.drK .rahProfileCm li p {color:#989b9f}.drK .rahProfileCm li:hover{box-shadow:none;transform:translate(0,0)}.rahPoprofileCm{display:flex;align-items:center;position:fixed;left:0;right:0;bottom:0;margin-bottom:-100%;z-index:20;transition:all .1s ease;width:100%;height:100%;opacity:0;visibility:hidden}.rahPoprofileCm_{width:100%;max-width:680px;max-height:calc(100% - 60px);border-radius:20px;transition:inherit;z-index:3;display:flex;overflow:hidden;position:relative;margin:0 auto;box-shadow:0 5px 30px 0 rgba(0,0,0,.05)}.rahPoprofileCm__{padding:35px 20px 20px;overflow-y:scroll;overflow-x:hidden;width:100%;background:#fffdfc}.rahPopcmck:checked ~ .rahPoprofileCm{margin-bottom:0;opacity:1;visibility:visible} .rahPopcmck:checked ~ .rahPoprofileCm .rahFcls{opacity:1;visibility:visible;background:rgba(0,0,0,.2);}@media screen and (max-width:768px){/* Pop-up */.rahPoprofileCm{transition:all .1s ease;align-items:flex-end} .rahPoprofileCm .rahPoprofileCm_{border-radius:20px 20px 0 0;max-width:100%;bottom:0;position:fixed;max-height:calc(100% - 60px);}}.rahPoprofileCm_:hover::after{content:\'Created by ICloudice.com\';font-weight:800;position:absolute;bottom:14px;right:10px;left:10px;font-size:12px;color:rgba(255,255,255,.8);width:auto;text-align:center;align-items:center;justify-content:center;padding:10px;background-color:#323232;border-radius:5px}.rahFcls{display:block;position:fixed;top:-50%;left:-50%;right:-50%;bottom:-50%;z-index:1;transition:all.1s ease;;background:transparent;opacity:0;visibility:hidden;-webkit-backdrop-filter:blur(1px);backdrop-filter:blur(1px);}", "appendChild", "head"];
+const rahpopcmjs = document[_0x285d[1]](_0x285d[0]);
+rahpopcmjs[_0x285d[2]] = _0x285d[3];
+rahpopcmjs[_0x285d[4]] = _0x285d[5];
+document[_0x285d[7]][_0x285d[6]](rahpopcmjs)
